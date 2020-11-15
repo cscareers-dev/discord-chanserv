7098483031eced0ed2bf204098d348e751842a2f
Joey Colon <joeycolon5@gmail.com>
2020-11-15 17:05:25 -0500

    feat: list users command (#2)

--- src/app/commands.ts
+++ src/app/commands.ts
@@ -1,3 +1,4 @@
+import { GuildChannel } from 'discord.js';
 import Logger from '../util/logger';
 import { MessagePayloadType } from './messages';
 
@@ -8,8 +9,54 @@ type CommandStoreType = {
   create: (payload: MessagePayloadType) => Promise<void>;
 };
 
-async function list(payload) {
-  Logger.info('list cmd');
+type ChannelListType = {
+  readonly name: string;
+  readonly user_count: number;
+  readonly channel: GuildChannel;
+};
+
+const COMMUNITY_CATEGORY = 'community channels';
+
+async function list(payload: MessagePayloadType) {
+  const { guild } = payload.source;
+  const communityChannels: ChannelListType[] = guild.channels.cache
+    .filter((channel) => {
+      const { parentID } = channel;
+      if (!parentID) {
+        return false;
+      }
+
+      const categoryName = (
+        guild.channels.cache.get(parentID)?.name || ''
+      ).toLowerCase();
+      if (!categoryName) {
+        return false;
+      }
+
+      return categoryName === COMMUNITY_CATEGORY;
+    })
+    .reduce((acc: ChannelListType[], channel) => {
+      const totalUsers = channel.permissionOverwrites.filter(
+        (permission) => permission.type === 'member',
+      ).size;
+      if (totalUsers === 0) {
+        return acc;
+      }
+
+      acc.push({
+        name: channel.name,
+        user_count: totalUsers,
+        channel: channel,
+      });
+
+      return acc;
+    }, [])
+    .sort((a, b) => b.user_count - a.user_count);
+
+  // TODO: Create user friendly response.
+  await payload.source
+    .reply(JSON.stringify({ communityChannels }))
+    .catch(Logger.error);
 }
 
 async function register(payload) {
