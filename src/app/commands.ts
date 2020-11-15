6ce621d0f3efdffb4105bfd89727534177be8a2f
joey <contact@joey.dev>
2020-11-15 14:27:52 -0500

    feat: list users command

--- src/app/commands.ts
+++ src/app/commands.ts
@@ -1,4 +1,4 @@
-import { TextChannel } from 'discord.js';
+import { GuildChannel, TextChannel } from 'discord.js';
 import Logger from '../util/logger';
 import { MessagePayloadType } from './messages';
 
@@ -9,29 +9,46 @@ type CommandStoreType = {
   create: (payload: MessagePayloadType) => Promise<void>;
 };
 
+type ChannelListType = {
+  readonly name: string;
+  readonly users: number;
+  readonly channel: GuildChannel;
+};
+
 const COMMUNITY_CATEGORY = 'community channels';
 
 async function list(payload: MessagePayloadType) {
   const { guild } = payload.source;
-  const communityChannels = guild.channels.cache.filter((channel) => {
-    const { parentID } = channel;
-    if (!parentID) {
-      return false;
-    }
+  const communityChannels: ChannelListType[] = guild.channels.cache
+    .filter((channel) => {
+      const { parentID } = channel;
+      if (!parentID) {
+        return false;
+      }
 
-    const categoryName = (
-      guild.channels.cache.get(parentID)?.name || ''
-    ).toLowerCase();
-    if (!categoryName) {
-      return false;
-    }
+      const categoryName = (
+        guild.channels.cache.get(parentID)?.name || ''
+      ).toLowerCase();
+      if (!categoryName) {
+        return false;
+      }
 
-    return categoryName === COMMUNITY_CATEGORY;
-  });
+      return categoryName === COMMUNITY_CATEGORY;
+    })
+    .map((channel) => ({
+      name: channel.name,
+      users: channel.permissionOverwrites.filter(
+        (permission) => permission.type === 'member',
+      ).size,
+      channel: channel,
+    }))
+    .filter((channel) => Boolean(channel.users))
+    .sort((a, b) => b.users - a.users);
 
-  communityChannels.forEach((channel) =>
-    Logger.info(channel.permissionOverwrites),
-  );
+  // TODO: Create user friendly response.
+  await payload.source
+    .reply(JSON.stringify({ communityChannels }))
+    .catch(Logger.error);
 }
 
 async function register(payload) {
