d5d7e5f92f233048b259593b7947da230f27e9d5
joey <contact@joey.dev>
2020-11-25 14:22:13 -0500

    fix: case sensitivity bug

--- src/app/commands/promote.command.ts
+++ src/app/commands/promote.command.ts
@@ -17,7 +17,7 @@ export default async function promote(payload: MessagePayloadType) {
     return;
   }
 
-  const user = payload.args.join(' ');
+  const user = payload.source.content.split(' ').slice(1).join(' ');
   if (!user) {
     await payload.source.reply('Invalid usage: `!promote user#1234`');
     return;
@@ -25,7 +25,9 @@ export default async function promote(payload: MessagePayloadType) {
 
   const isValidUser = Boolean(fetchUser(user, payload.source.guild));
   if (!isValidUser) {
-    await payload.source.reply(`Invalid username: \`${user}\``);
+    await payload.source.reply(
+      `Invalid username (case sensitive): \`${user}\``,
+    );
     return;
   }
 
