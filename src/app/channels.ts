d6376edc957a7759e732a26a7c53bb38fc11feb7
joey <contact@joey.dev>
2020-11-26 12:14:48 -0500

    feat: support multiple community channel categories

--- src/app/channels.ts
+++ src/app/channels.ts
@@ -1,4 +1,4 @@
-import { Guild, GuildChannel, TextChannel } from 'discord.js';
+import { CategoryChannel, Guild, GuildChannel, TextChannel } from 'discord.js';
 import Constants from '../constants';
 import { Maybe } from '../util/types';
 import { DiscordMessageType } from './messages';
@@ -16,26 +16,65 @@ type ChannelListType = {
 
 export const INVALID_CHANNEL_NAMES = new Set(['instructions']);
 export const BOT_COMMANDS_CHANNEL = Constants.BotCommandsChannel;
-export const COMMUNITY_CATEGORY = Constants.CommunityCategory;
+export const COMMUNITY_CATEGORY_PREFIX = Constants.CommunityCategoryPrefix;
+const MAX_CHANNEL_LIMIT = 50;
 
 export const createChannel = async (
   request: ChannelRequestType,
   guild: Maybe<Guild>,
 ) => {
-  const communityCategory = guild?.channels.cache.find(
-    (channel) => channel.name === COMMUNITY_CATEGORY,
-  );
-  if (!communityCategory) {
+  if (!guild) {
+    throw new Error(`Unable to locate guild ${JSON.stringify(request)}`);
+  }
+
+  // Since Discord categories are only allowed to have at most 50 child-channels, we need to determine
+  // which available categories we can create this channel in.
+  const availableCategories = guild.channels.cache
+    .filter((channel) => channel.name.startsWith(COMMUNITY_CATEGORY_PREFIX))
+    .reduce((acc, channel) => {
+      acc[channel.name] = [0, channel as CategoryChannel];
+      return acc;
+    }, {} as Record<string, [number, CategoryChannel]>);
+
+  if (Object.keys(availableCategories).length === 0) {
     throw new Error(
-      `Unable to locate community category ${JSON.stringify(request)}`,
+      `Unable to locate community categories ${JSON.stringify(request)}`,
     );
   }
 
-  return (await guild?.channels.create(request.channelName, {
+  fetchCommunityChannels(guild)
+    .map((channel) => channel.channel)
+    .forEach((channel) => {
+      const categoryName = guild.channels.cache
+        .get(channel.parentID || '')
+        ?.name.toLowerCase();
+      if (!categoryName) {
+        return false;
+      }
+      const [channelCount, categoryChannel] = availableCategories[categoryName];
+      availableCategories[categoryName] = [channelCount + 1, categoryChannel];
+    });
+
+  const availableCategoryName = Object.keys(availableCategories).find(
+    (category) => {
+      const [channelCount] = availableCategories[category];
+      return channelCount < MAX_CHANNEL_LIMIT;
+    },
+  );
+
+  if (!availableCategoryName) {
+    throw new Error(
+      `Unable to find a non-full category ${JSON.stringify(request)}`,
+    );
+  }
+
+  const [, communityCategory] = availableCategories[availableCategoryName];
+
+  return await guild.channels.create(request.channelName, {
     type: 'text',
     topic: `Channel Admins: ${request.user}`,
     parent: communityCategory,
-  })) as Maybe<TextChannel>;
+  });
 };
 
 export const isFromBotChannel = (message: DiscordMessageType) =>
@@ -79,7 +118,7 @@ export const fetchCommunityChannels = (
         return false;
       }
 
-      return categoryName === COMMUNITY_CATEGORY;
+      return categoryName.startsWith(COMMUNITY_CATEGORY_PREFIX);
     })
     .map((channel) => ({
       name: channel.name,
