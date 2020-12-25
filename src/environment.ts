d1d73d13b98ed6b23c18068b50756f8ae30588a1
joey <contact@joey.dev>
2020-12-24 19:00:39 -0500

    feat: sandbox analytics client

--- src/environment.ts
+++ src/environment.ts
@@ -6,6 +6,7 @@ config({ path: resolve(__dirname, '../.env') });
 const Environment = {
   DiscordToken: process.env.DISCORD_TOKEN,
   SegmentToken: process.env.SEGMENT_TOKEN,
+  Playground: process.env.NODE_ENV,
 };
 
 export default Environment;
